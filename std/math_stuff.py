3ad146e6c1bc59ca9ed77237c56c4ddccc289482
Leoni <leoni.dessau@mailbox.org>
2025-11-06 11:11:51 +0100

    reduced chi2

--- std/math_stuff.py
+++ std/math_stuff.py
@@ -1,2 +1,12 @@
+import numpy as np
+import std
+
+
 def add(a, b):
     return a + b
+
+
+def reduced_chi_2(data, fit, params, sigma=None):
+    dof = len(data) - len(params)
+    var = np.sqrt(np.var(data)) if std.util.none(sigma) else sigma
+    return sum(((fit - data) / var) ** 2) / dof
