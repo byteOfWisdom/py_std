133c7e1e745461592f92821c208799c8fba17bb4
Leoni <leoni.dessau@mailbox.org>
2025-11-14 17:50:52 +0100

    huh i guess i changed things

--- std/math_stuff.py
+++ std/math_stuff.py
@@ -1,5 +1,7 @@
 import numpy as np
 import std
+import scipy
+import inspect
 
 
 def add(a, b):
@@ -16,3 +18,34 @@ def goodness_of_fit(data, fit):
     rss = sum((data - fit) ** 2)
     tss = sum((data - np.average(data)) ** 2)
     return 1 - (rss / tss)
+
+
+def res_sq(f, x, y):
+    return sum((f(x) - y) ** 2)
+
+
+def make_initial_guesses(f, x, y, argc):
+    # first ensure area under curve is somewhat similar
+    return []
+
+
+def gaussian(x, amp, mu, sigma):
+    temp_a = (x - mu) ** 2
+    temp_b = 2 * (sigma ** 2)
+    return amp * np.exp(-temp_a / temp_b)
+
+
+def double_gaussian(x, a1, a2, mu1, mu2, sigma1, sigma2, const):
+    return gaussian(x, a1, mu1, sigma1) + gaussian(x, a2, mu2, sigma2) + const
+
+
+def fit_func(func, x_values, y_values, x_errors=None, y_errors=None, p0=None):
+    model = scipy.odr.Model(lambda B, x: func(x, *B))
+    data = scipy.odr.RealData(x_values, y_values, x_errors, y_errors)
+    if std.none(p0):
+        func_args = len(inspect.getfullargspec(func))
+        p0 = np.zeros(func_args)
+        p0 += 1
+    odr_run = scipy.odr.ODR(data, model, beta0=p0, maxit=750)
+    odr_run.run()
+    return odr_run.output
