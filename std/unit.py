e4d1df2b8f7b38ad308bb924317862dcc1da8ec3
Leoni <leoni.dessau@mailbox.org>
2025-12-22 12:06:29 +0100

    more consts

--- std/unit.py
+++ std/unit.py
@@ -1,3 +1,5 @@
+import math
+
 m = 1
 nm = 1e-9
 um = 1e-6
@@ -9,7 +11,11 @@ electron_charge = 1.602176634e-19
 vacuum_permitivity = 8.8541878188e-12
 boltzmann_const = 1.380649e-23
 
+planck_const = 6.62607015e-34
+reduced_planck_const = planck_const / math.pi
+planck_const_eV = planck_const / eV
+reduced_planck_const_eV = reduced_planck_const / eV
+
 electron_mass = 9.1093837139e-31
 proton_mass = 1.67262192595e-27
 neutron_mass = 1.67492750056e-27
-
