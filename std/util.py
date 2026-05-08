2aef011613b7066543aa61b4abe7f9b69ca6f914
Leoni <leoni.dessau@mailbox.org>
2026-05-08 18:12:25 +0200

    dont ask about mesh pls

--- std/util.py
+++ std/util.py
@@ -10,10 +10,22 @@ def some(x):
     return not none(x)
 
 
-def mesh(a, b):
-    for first in a:
-        for second in b:
-            yield first, second
+def mesh(a, b, c=None, d=None):
+    if some(c) and some(d):
+        for first in a:
+            for second in b:
+                for third in c:
+                    for fourth in d:
+                        yield first, second, third, fourth
+    elif some(c):
+        for first in a:
+            for second in b:
+                for third in c:
+                    yield first, second, third
+    else:
+        for first in a:
+            for second in b:
+                yield first, second
 
 
 def load_csv(filename, delimiter=" ", skiprows=0):
