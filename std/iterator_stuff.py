2137185081ca7d48ab58ed40cf646db8f9b33c52
Leoni <leoni.dessau@mailbox.org>
2026-05-09 00:23:33 +0200

    fix

--- std/iterator_stuff.py
+++ std/iterator_stuff.py
@@ -3,13 +3,13 @@ def pairs(iter):
 
 
 def mesh(a, b, c=None, d=None):
-    if some(c) and some(d):
+    if c and d:
         for first in a:
             for second in b:
                 for third in c:
                     for fourth in d:
                         yield first, second, third, fourth
-    elif some(c):
+    elif c:
         for first in a:
             for second in b:
                 for third in c:
