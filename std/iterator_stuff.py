252fe1ef7a40b49e2c25011822e0727c3ccf043b
Leoni <leoni.dessau@mailbox.org>
2026-05-08 21:57:28 +0200

    asdf

--- std/iterator_stuff.py
+++ std/iterator_stuff.py
@@ -1,2 +1,21 @@
 def pairs(iter):
     return None
+
+
+def mesh(a, b, c=None, d=None):
+    if some(c) and some(d):
+        for first in a:
+            for second in b:
+                for third in c:
+                    for fourth in d:
+                        yield first, second, third, fourth
+    elif some(c):
+        for first in a:
+            for second in b:
+                for third in c:
+                    yield first, second, third
+    else:
+        for first in a:
+            for second in b:
+                yield first, second
+
--- std/util.py
+++ std/util.py
@@ -10,24 +10,6 @@ def some(x):
     return not none(x)
 
 
-def mesh(a, b, c=None, d=None):
-    if some(c) and some(d):
-        for first in a:
-            for second in b:
-                for third in c:
-                    for fourth in d:
-                        yield first, second, third, fourth
-    elif some(c):
-        for first in a:
-            for second in b:
-                for third in c:
-                    yield first, second, third
-    else:
-        for first in a:
-            for second in b:
-                yield first, second
-
-
 def load_csv(filename, delimiter=" ", skiprows=0):
     import numpy as np
     with open(filename, "r") as file:
